bd168aea6ef4079d0a3624953c0384dbf40c13b6
unknown <ajameslusk@gmail.com>
2018-07-09 12:30:37 -0400

    created user schema/model

---
+++ users/userModel.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+mongoose.promise = global.Promise;
+
+const UserSchema = mongoose.Schema({
+  displayName:{
+    type: String,
+    required: true,
+  },
+  username:{
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email:{
+    type: String,
+    unique: true,
+  },
+  password:{
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.methods.serialize = function() {
+  return {
+    username: this.username || '',
+    displayName: this.name || '',
+    id: this._id,
+  };
+};
+
+UserSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+UserSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 8);
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = { User };
