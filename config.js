b6aa3741169f0550265a0975f747039444654402
unknown <ajameslusk@gmail.com>
2018-07-18 12:26:16 -0400

    removed dotenv config

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  CLIENT_ORIGIN_2: process.env.CLIENT_ORIGIN_2 || 'http://localhost:3001',
   MONGODB_URI:
         process.env.MONGODB_URI,
   TEST_DATABASE_URL:
